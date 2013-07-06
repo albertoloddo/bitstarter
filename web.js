ede58f247976a14c610a46fd7cc07f7c2e98f8b2
albertoloddo <a.loddo3@gmail.com>
2013-07-06 18:42:55 +0000

    funziona cazzo

--- web.js
+++ web.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var fs = require('fs');
-var buf = new Buffer(256);
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html',buf.toString('utf-8'));
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+                                                                                                                                                                                                                  
+var express = require('express');                                                                                                                                                                                  
+var fs = require('fs');                                                                                                                                                                                            
+                                                                                                                                                                                         
+var app = express.createServer(express.logger());                                                                                                                                                                  
+                                                                                                                                                                                                                   
+app.get('/', function(request, response) {                                                                                                                                                                         
+  response.send(fs.readFileSync('index.html',buffer.toString('utf-8',0,len));                                                                                                                                               
+});                                                                                                                                                                                                                
+                                                                                                                                                                                                                   
+var port = process.env.PORT || 5000;                                                                                                                                                                               
+app.listen(port, function() {                                                                                                                                                                                      
+  console.log("Listening on " + port);                                                                                                                                                                             
 });
